8b04ddee24874b6268853e7e3780a33a6cc16691
stavratum <89344034+stavratum@users.noreply.github.com>
2024-09-18 06:18:18 +0500

    Add files via upload

--- commands/temp.go
+++ commands/temp.go
@@ -3,10 +3,20 @@ package commands
 import (
 	"eco/eco"
 	"eco/handler"
-	"log"
+	"fmt"
+	"strconv"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
+
+var TempDescription = `
+> **%s**
+> ` + "`%s`" + `
+
+**Duration**: ` + "`%s`" + `
+**Updated**: %s 
+`
 
 var Temp = &handler.Command{
 	Name:        "temp",
@@ -26,63 +36,21 @@ var Temp = &handler.Command{
 		}
 
 		channel, ok := handler.TempChannels[m.ChannelID]
-		if !ok {
+		if ok {
+			if channel.Schedule++; channel.Schedule > handler.ChannelScheduleNSFW {
+				channel.Schedule = handler.NoSchedule
+			}
+
+			channel.Time = time.Now()
+			channel.Messages = map[string]time.Time{}
+		} else {
 			channel = handler.NewTempChannel()
 			handler.TempChannels[m.ChannelID] = channel
-
-			s.ChannelMessageSendReply(m.ChannelID, "Temp channel type: Manual.", m.Reference())
-			return
 		}
 
-		channel.Type++
-		content := "Temp channel type: Everything."
-
-		switch channel.Type {
-		case handler.Everything:
-		case handler.EverythingNSFW:
-			content = "Temp channel type: Everything (NSFW)."
-		case 3:
-			delete(handler.TempChannels, m.ChannelID)
-			content = "Channel is not temp anymore."
-		}
-
-		_, err = s.ChannelMessageSendComplex(m.ChannelID, &discordgo.MessageSend{
-			Content:   content,
-			Reference: m.Reference(),
-			Components: []discordgo.MessageComponent{
-				discordgo.ActionsRow{
-					Components: []discordgo.MessageComponent{
-						discordgo.Button{
-							// Label is what the user will see on the button.
-							Label: "Yes ($999.99)",
-							// Style provides coloring of the button. There are not so many styles tho.
-							Style: discordgo.SuccessButton,
-							// Disabled allows bot to disable some buttons for users.
-							Disabled: false,
-							// CustomID is a thing telling Discord which data to send when this button will be pressed.
-							CustomID: "fd_yes",
-
-							Emoji: &discordgo.ComponentEmoji{
-								Name: "🤑",
-							},
-						},
-						discordgo.Button{
-							Label:    "No ($299.99)",
-							Style:    discordgo.DangerButton,
-							Disabled: false,
-							CustomID: "fd_no",
-
-							Emoji: &discordgo.ComponentEmoji{
-								Name: "😡",
-							},
-						},
-					},
-				},
-			},
-		})
-
-		if err != nil {
-			log.Println(err.Error())
-		}
+		s.ChannelMessageSendEmbedReply(m.ChannelID, &discordgo.MessageEmbed{
+			Description: fmt.Sprintf(TempDescription, channel.Schedule.String(), channel.Schedule.Description(), channel.Duration.String(), "<t:"+strconv.FormatInt(channel.Time.Unix(), 10)+":R>"),
+			Color:       0xff5858,
+		}, m.Reference())
 	},
 }
