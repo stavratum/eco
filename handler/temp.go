c3e363fb1322007f9d3eceeebbb9e3ebe78dffc6
stavratum <fly3xorjustik@gmail.com>
2024-09-18 06:17:30 +0500

    Finish some commands

--- handler/temp.go
+++ handler/temp.go
@@ -7,29 +7,69 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+type Schedule uint8
+
 const (
-	Manual         = iota // for manual deleting of messages (bulk)
-	Everything            // planned for deleting the channel itself
-	EverythingNSFW        // planned for deleting the channel itself, but making it nsfw beforehand (idk whats the point honestly i might remove)
+	NoSchedule                Schedule = iota
+	MessageSchedule                    // for deleting all messages
+	MessageAttachmentSchedule          // for deleting all messages but with attachments
+	ChannelSchedule                    // for deleting the channel itself
+	ChannelScheduleNSFW                // for deleting the channel itself, but making it nsfw beforehand (idk whats the point honestly i might remove)
 )
 
-// temporary channel that stores umm yea
+func (s Schedule) String() string {
+	switch s {
+	case NoSchedule:
+		return "No Schedule"
+	case MessageSchedule:
+		return "Message Schedule"
+	case MessageAttachmentSchedule:
+		return "Message Attachment Schedule"
+	case ChannelSchedule:
+		return "Channel Schedule"
+	case ChannelScheduleNSFW:
+		return "Channel Schedule NSFW"
+	}
+
+	return "Undefined"
+}
+
+func (s Schedule) Description() string {
+	switch s {
+	case NoSchedule:
+		return "Schedules nothing."
+	case MessageSchedule:
+		return "Schedules every message sent to be deleted after a specified amount time."
+	case MessageAttachmentSchedule:
+		return "Schedules every message sent with any kind of attachment to be deleted after a specified amount time."
+	case ChannelSchedule:
+		return "Schedules channel to be deleted and replaced with the exact copy after a specified amount of time."
+	case ChannelScheduleNSFW:
+		return "Schedules channel to be made age restricted and deleted and replaced with the exact copy after a specified amount of time."
+	}
+
+	return "Undefined"
+}
+
 type TempChannel struct {
-	Type  uint8
-	Timed time.Time
+	Schedule Schedule
+	Time     time.Time
+	Duration time.Duration
 
 	Messages map[string]time.Time
 }
 
 func NewTempChannel() *TempChannel {
 	return &TempChannel{
-		Type:     Manual,
+		Schedule: NoSchedule,
+		Time:     time.Now(),
+		Duration: time.Minute,
+
 		Messages: map[string]time.Time{},
 	}
 }
 
 var TempChannels = map[string]*TempChannel{}
-var TempDuration = time.Minute               // how long temp messages or channels stay
 var TempTicker = time.NewTicker(time.Minute) // update every minute
 var TempInit = false
 
@@ -44,15 +84,14 @@ func ready(s *eco.Session) {
 		<-TempTicker.C
 
 		for cID, channel := range TempChannels {
-			switch channel.Type {
-			case Manual:
-				var (
-					now      = time.Now()
-					messages = []string{}
-				)
+			now := time.Now()
 
-				for mID, timed := range channel.Messages {
-					if !now.After(timed) {
+			switch channel.Schedule {
+			case MessageSchedule, MessageAttachmentSchedule:
+				messages := []string{}
+
+				for mID, time := range channel.Messages {
+					if now.Sub(time) < channel.Duration {
 						continue
 					}
 
@@ -65,63 +104,86 @@ func ready(s *eco.Session) {
 				}
 
 				s.ChannelMessagesBulkDelete(cID, messages)
-			case Everything:
-				// TODO: recreate a channel and delete old one
 
-			case EverythingNSFW:
-				// TODO: recreate a channel, make old one nsfw and delete it
+			case ChannelSchedule, ChannelScheduleNSFW:
+				if channel.Time.Add(channel.Duration).Before(now) {
+					continue
+				}
 
+				c, err := s.State.Channel(cID)
+				if err != nil {
+					if c, err = s.Channel(cID); err != nil {
+						s.ChannelMessageSend(cID, err.Error())
+						continue
+					}
+
+					s.State.ChannelAdd(c)
+				}
+
+				if channel.Schedule == ChannelScheduleNSFW {
+					c.NSFW = true
+
+					if _, err := s.ChannelEdit(cID, &discordgo.ChannelEdit{NSFW: &c.NSFW}); err != nil {
+						s.ChannelMessageSend(cID, err.Error())
+						continue
+					}
+				}
+
+				if _, err = s.ChannelDelete(cID); err != nil {
+					s.ChannelMessageSend(cID, err.Error())
+					continue
+				}
+
+				new, err := s.GuildChannelCreateComplex(c.GuildID, discordgo.GuildChannelCreateData{
+					Name:                 c.Name,
+					Type:                 c.Type,
+					Topic:                c.Topic,
+					Bitrate:              c.Bitrate,
+					UserLimit:            c.UserLimit,
+					RateLimitPerUser:     c.RateLimitPerUser,
+					Position:             c.Position,
+					PermissionOverwrites: c.PermissionOverwrites,
+					ParentID:             c.ParentID,
+					NSFW:                 c.NSFW,
+				})
+				if err != nil {
+					s.ChannelMessageSend(cID, err.Error())
+					continue
+				}
+
+				TempChannels[new.ID] = channel
+				channel.Messages = map[string]time.Time{}
+				channel.Time = time.Now()
 			}
 		}
 	}
-}
-
-// when message gets created make it temp as long as its configured
-func messageCreate(m *discordgo.MessageCreate) {
-	channel, ok := TempChannels[m.ChannelID]
-	if !ok {
-		return
-	}
-
-	if channel.Type != Manual {
-		return
-	}
-
-	channel.Messages[m.ID] = time.Now().Add(TempDuration)
-}
-
-// when message gets deleted forget it
-func messageDelete(m *discordgo.MessageDelete) {
-	channel, ok := TempChannels[m.ChannelID]
-	if !ok {
-		return
-	}
-
-	if channel.Type != 0 {
-		return
-	}
-
-	delete(channel.Messages, m.ID)
-}
-
-// when channel deletes forget everything about it
-func channelDelete(c *discordgo.ChannelDelete) {
-	if _, ok := TempChannels[c.ID]; !ok {
-		return
-	}
-
-	delete(TempChannels, c.ID)
 }
 
 func OnTemp(s *eco.Session, i interface{}) {
 	switch e := i.(type) {
 	case *discordgo.Ready:
 		ready(s)
+
 	case *discordgo.MessageCreate:
-		messageCreate(e)
+		if channel, ok := TempChannels[e.ChannelID]; ok {
+			switch channel.Schedule {
+			case MessageAttachmentSchedule:
+				if len(e.Attachments) == 0 {
+					return
+				}
+
+				fallthrough
+			case MessageSchedule:
+				channel.Messages[e.ID] = time.Now()
+			}
+		}
+
 	case *discordgo.MessageDelete:
-		messageDelete(e)
+		if channel, ok := TempChannels[e.ChannelID]; ok {
+			delete(channel.Messages, e.ID)
+		}
+
 	case *discordgo.ChannelDelete:
-		channelDelete(e)
+		delete(TempChannels, e.ID)
 	}
 }
